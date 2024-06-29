ebb34af672eb375dfc1c9064b01f88641b7a94c5
victoriacmarques <victoriacmarques.dev@gmail.com>
2024-06-29 00:08:07 -0300

    Olivia mais gay ainda

--- src/dados/Domestico.java
+++ src/dados/Domestico.java
@@ -1,4 +1,28 @@
 package dados;
 
-public class Domestico {
+public class Domestico extends Robo {
+    private int nivel;
+
+    public Domestico(int id, String modelo, double valorDiario, int nivel) {
+        super(id, modelo, valorDiario);
+        this.nivel = nivel;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    @Override
+    public double calculaLocacao(int dias) {
+        return dias * getValorDiario();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Nível: " + nivel;
+    }
 }
